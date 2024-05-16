052c9064525cf9d94321850972cea14967d98272
Austin Maybin <austinmaybin@gmail.com>
2024-05-15 21:27:10 -0400

    adding score function

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3)
+    getRandom = Math.floor(Math.random() * 3);
     
     if (getRandom === 0){
         return "Rock";
     }
         
     else if (getRandom === 1){
-        return"Paper";
+        return "Paper";
     }
 
     else {
-        return"Scissors"
+        return"Scissors";
     }
+}   
 
+function getHumanChoice(){
+    let choice = prompt("Do you choose rock, paper, or scissors?");
+    return choice;
+}
+console.log(getHumanChoice());
+
+function humanScore(){
+
+}
+
+function computerScore(){
     
-    
-
-
-
-
 }
